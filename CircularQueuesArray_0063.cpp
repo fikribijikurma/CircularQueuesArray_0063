8dfd451cc6bb4506d82832826f60e12f4b5cbdc9
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-31 01:07:42 -0700

    manambahkan fungtion pada Queues

--- CircularQueuesArray_0063.cpp
+++ CircularQueuesArray_0063.cpp
@@ -86,6 +86,20 @@ public:
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+
+			FRONT_position = 0;
+
+			// Iterasi dari awal array hingga REAR
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
+
+int main() {
+	Queues q;
+	char ch;
+}
